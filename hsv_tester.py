40f6fb5d971a180fbb61c786f390d1151713410f
richie <rgkirch@gmail.com>
2018-03-05 22:29:36 -0500

    broke the hue tester

--- hsv_tester.py
+++ hsv_tester.py
@@ -3,18 +3,46 @@ import numpy as np
 import imageio
 
 frame = cv2.imread('img.png')
+hue = 0
+saturation = 0
+value = 0
 
 
-def callback(h):
+def hue_callback(x):
+    global hue
+    hue = x
+    update()
+
+
+def saturation_callback(x):
+    global saturation
+    saturation = x
+    update()
+
+
+def value_callback(x):
+    global value
+    value = x
+    update()
+
+
+def update():
     global frame
+    global hue
+    global saturation
+    global value
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    hsv[:, :, 0] = h
+    hsv[:, :, 0] = hue
+    hsv[:, :, 1] = saturation
+    hsv[:, :, 2] = value
     frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     cv2.imshow('win', frame)
 
 
 cv2.namedWindow('win')
-cv2.createTrackbar('hue', 'win', 0, 180, callback)
+cv2.createTrackbar('hue', 'win', 0, 180, hue_callback)
+cv2.createTrackbar('saturation', 'win', 0, 255, saturation_callback)
+cv2.createTrackbar('value', 'win', 0, 255, value_callback)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
