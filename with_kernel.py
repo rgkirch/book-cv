b41aaa36c48ef157c134491c690caa23bafe76d9
richie <rgkirch@gmail.com>
2018-03-08 18:11:03 -0500

    wide and tall kernel test

--- with_kernel.py
+++ with_kernel.py
@@ -6,20 +6,26 @@ import sys
 import json
 
 
+def update(dummy=None):
+    wide = cv.getTrackbarPos('wide', 'img')
+    tall = cv.getTrackbarPos('tall', 'img')
+    wide_kernel = np.ones((1, wide))
+    tall_kernel = np.ones((tall, 1))
+    wide_img = cv.bitwise_not(cv.dilate(cv.bitwise_not(img), wide_kernel))
+    tall_img = cv.bitwise_not(cv.dilate(cv.bitwise_not(img), tall_kernel))
+    or_img = cv.bitwise_not(cv.bitwise_or(
+        cv.bitwise_not(wide_img), cv.bitwise_not(tall_img)))
+    cv.imshow('img', img)
+    cv.imshow('wide_img', wide_img)
+    cv.imshow('tall_img', tall_img)
+    cv.imshow('or_img', or_img)
+
+
 img = cv.imread(sys.argv[1], 0)
-wide = np.ones((1, 5))
-print(wide)
-tall = np.ones((5, 1))
-print(tall)
 img = cv.adaptiveThreshold(
     img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-# img = cv.morphologyEx(img, cv.MORPH_OPEN, np.ones((2, 2)))
 cv.imshow('img', img)
-wide = cv.bitwise_not(cv.dilate(cv.bitwise_not(img), wide))
-tall = cv.bitwise_not(cv.dilate(cv.bitwise_not(img), tall))
-cv.imshow('wide', wide)
-cv.imshow('tall', tall)
-bitwise_or = cv.bitwise_not(cv.bitwise_or(
-    cv.bitwise_not(wide), cv.bitwise_not(tall)))
-cv.imshow('or', bitwise_or)
+cv.createTrackbar('wide', 'img', 1, 10, update)
+cv.createTrackbar('tall', 'img', 1, 10, update)
+update()
 cv.waitKey(0)
