f9e14283322695acc3da95797e8ebff545084a14
richie <rgkirch@gmail.com>
2018-03-09 18:01:31 -0500

    real lines now

--- detect_lines.py
+++ detect_lines.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import sys
 import matplotlib.pyplot as plt
 from collections import defaultdict
+import numpy as np
+import math
 
 
 def f(l):
@@ -17,24 +19,15 @@ def f(l):
 
 
 def get(w, h):
-    segment_lengths = sorted(set([w // x
-                                  for x in range(1, h)]), reverse=True)
-    segment_lengths += sorted(set([w //
-                                   x for x in range(-h, 0)]), reverse=True)
-    data = []
-    for segment_length in segment_lengths:
-        num_changes = 0
-        # print("segment", segment_length)
-        # print("hi")
-        for y_offset in range(h):
-            # print("y_offset", y_offset)
-            for row_y_start in range(w//segment_length):
-                pxs = []
-                pxs.extend(img[(row_y_start+y_offset) % h][row_y_start *
-                                                           segment_length: (row_y_start+1)*segment_length])
-                num_changes += f(pxs)
-        data.append(num_changes)
-    return (segment_lengths, data)
+    res = 100
+    the = []
+    for slope in [math.tan(x / res * math.pi) for x in range(res)]:
+        data = []
+        for offset in range(h):
+            for x, y in [(x, int(slope * x + .5)) for x in range(w)]:
+                data.append(img[(y+offset) % h][x])
+        the.append(data)
+    return the
 
 
 img = cv.imread(sys.argv[1], 0)
@@ -42,12 +35,13 @@ img = cv.adaptiveThreshold(
     img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
 # print(img[0][0:2])
 h, w = img.shape[:2]
-segment_lengths, data = get(w, h)
-print("data")
-print(data)
-print("segs")
-print(segment_lengths)
-cv.imshow('img', img)
-plt.plot(segment_lengths, [k / h for k in data])
+data = get(w, h)
+# print("data")
+# print(data)
+# print("segs")
+# print(segment_lengths)
+# cv.imshow('img', img)
+# cv.imshow('transs', np.transpose(img))
+plt.plot(data)
 plt.show()
 # cv.waitKey(0)
