207f12b31238e279a0171d0e201b102365f3ec2f
richie <rgkirch@gmail.com>
2018-03-10 08:02:27 -0500

    floodfill works

--- floodfill.py
+++ floodfill.py
@@ -10,20 +10,11 @@ def update(dummy=None):
     if seed_pt is None:
         cv.imshow('image', img)
         return
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (sz, sz))
-    copy = img.copy()
+    copy = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     mask[:] = 0
-    # cv.bitwise_and(flooded, flooded, mask=mask)
-    # green.copy_to(flooded, mask)
-    copy = cv.morphologyEx(copy, cv.MORPH_CLOSE, kernel)
-    copy = cv.erode(copy, kernel, iterations=iters)
-    cv.floodFill(copy, mask, seed_pt, (255, 255, 255),
+    cv.floodFill(copy, mask, seed_pt, (200, 50, 50),
                  (1,)*3, (1,)*3, cv.FLOODFILL_FIXED_RANGE | 4 | (255 << 8))
-    cv.circle(copy, seed_pt, 2, (0, 0, 255), -1)
-    cv.circle(mask, seed_pt, 2, (0, 0, 255), -1)
-    # cv.imshow('floodfill', flooded)
-    cv.imshow('mask', mask)
-    cv.imshow('copy', copy)
+    cv.imshow('image', copy)
 
 
 def onmouse(event, x, y, flags, param):
@@ -36,17 +27,13 @@ def onmouse(event, x, y, flags, param):
 img = cv.imread(sys.argv[1], 0)
 img = cv.adaptiveThreshold(
     img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-h, w = img.shape[:2]
-green = np.zeros((w, h, 3), np.uint8)
-green[:] = (0, 255, 0)
+h, w = img.shape
 mask = np.zeros((h+2, w+2), np.uint8)
 seed_pt = None
 update()
-cv.createTrackbar('sz', 'image', 3, 10, update)
+cv.createTrackbar('sz', 'image', 2, 10, update)
 cv.createTrackbar('iters', 'image', 1, 10, update)
 cv.setMouseCallback('image', onmouse)
-cv.setMouseCallback('mask', onmouse)
-cv.setMouseCallback('copy', onmouse)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
